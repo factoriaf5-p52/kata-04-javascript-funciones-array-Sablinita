fb70120c62fa7d6aa7b330865ab623abc732f777
Sablinita <sablinahare@gmail.com>
2022-11-21 01:14:01 +0100

    done what I could

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,16 +1,54 @@
-/* Encuentra el máximo */
+/* Encuentra el máximo 1*/
 
-export function maxOfTwoNumbers(){}
+export function maxOfTwoNumbers(num1, num2){
+    if (num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+/* Encuentra la más larga 2*/
+
 export function findLongestWord(){}
-export function sumArray(){}
-export function averageNumbers(){}
+
+
+
+
+
+/*Calcula la suma de un array 3*/
+
+
+export function sumArray(array){
+    let addition = 0; 
+    for (let i=0; i<array.length; i++){
+     addition = array[i] + addition;
+    }
+    return addition;
+}
+
+
+
+/*Calcula el promedio de un array 4*/
+
+
+export function averageNumbers(list){
+
+    const additionList = list.reduce(function(valuesAc = 0, newValues){
+        return valuesAc + newValues;
+});
+
+    const average = additionList / list.length;
+
+    return average;
+    }
+
+  
+  
+/*Calcula el promedio de un palabra 5*/
+
 export function averageWordLength(){}
 export function uniquifyArray(){}
 export function doesWordExist(){}
 export function howManyTimes(){}
 export function greatestProduct(){}
-
-
-
-
-
